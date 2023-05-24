3d83319ef82e66a79ea618a8b05efd42c46579e5
mingyu ma <mashiro980107@gmail.com>
2023-05-24 17:30:34 +0900

    新增环形缓冲区

--- Server/Common/RingBuffer.cs
+++ Server/Common/RingBuffer.cs
@@ -11,7 +11,7 @@ namespace Server.Common
         private int tail;
         private int size;
 
-        public RingBuffer(int size)
+        public RingBuffer(int size = 4096)
         {
             buffer = new byte[size];
             this.size = size;
@@ -34,5 +34,45 @@ namespace Server.Common
             buffer[tail] = data;
             tail = (tail + 1) % size;
         }
+
+        public byte Read()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Buffer is empty");
+            }
+
+            byte result = buffer[head];
+            head = (head + 1) % size;
+
+            return result;
+        }
+
+        public void Write(byte[] data)
+        {
+            foreach(byte bytedata in data)
+            {
+                Write(bytedata);
+            }
+        }
+
+        public byte[] Read(int length)
+        {
+            if (length > size)
+            {
+                throw new ArgumentException("Length is larger than buffer size");
+            }
+
+            byte[] result = new byte[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = Read();
+            }
+
+            return result;
+        }
+
+
     }
 }
