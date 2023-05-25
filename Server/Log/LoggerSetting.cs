66fe0b5922127cdd8951ec30443d71e222be95d1
maomaoyu1999 <mashiro980107@gmail.com>
2023-05-25 23:01:51 +0900

    新增Const

---
+++ Server/Log/LoggerSetting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Server.Common;
+
+namespace Server.Log
+{
+    public class LoggerSetting : SettingBase
+    {
+        public LogLevel ConsoleWriteLevel { get; set; }
+
+        public LogLevel FileWriteLevel { get; set; }
+
+        public bool IsWriteToFile { get; set; }
+    }
+}
