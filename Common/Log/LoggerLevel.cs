35a36a0d897d0c7188bbd7dd42d56a20ea3860c8
maomaoyu1999 <mashiro980107@gmail.com>
2023-05-28 18:15:46 +0900

    分离公共类

---
+++ Common/Log/LoggerLevel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Server.Log
+{
+    /// <summary>
+    /// Log Level
+    /// </summary>
+    public enum LogLevel
+    {
+        ALL,
+
+        DEBUG,
+
+        INFO,
+
+        WARNING,
+
+        ERROR,
+
+        OFF,
+    }
+}
